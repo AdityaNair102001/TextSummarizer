577841dd816c6cff7538925946e6f887871556cd
AdityaNair102001 <adityanair102001@gmail.com>
2021-02-06 10:54:22 +0530

    successful data exchange

--- main.py
+++ main.py
@@ -3,34 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template
 
 import nltk
+nltk.download('punkt')
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 
+
 # Define a flask app
-app = Flask(__name__)
-
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-    if request.method == 'POST':
-        print(request.form['input'])
-        answer = textSummarizer(request.form['input'])
-        return answer
-
-    return render_template('index.html')
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    app.run()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 def textSummarizer(textInput):
     # Input text - to summarize
@@ -80,5 +62,25 @@ def textSummarizer(textInput):
         if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
             summary += " " + sentence
 
-    print("The summary is")
     return summary
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        print(request.form['input'])
+        answer = textSummarizer(request.form['input'])
+        Message = {'message': answer}
+        return Message
+
+    return render_template('index.html')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
